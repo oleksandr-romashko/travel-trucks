0883f7c0f9d16b08cdd0313f86181e8f44fd58a9
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-10-29 00:52:43 +0100

    Fix rating scale coloring

--- src/components/UI/Rating/Rating.jsx
+++ src/components/UI/Rating/Rating.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+
 import { Icon } from "@/components";
+
 import css from "./Rating.module.css";
 
 const Rating = ({ rating }) => {
   return (
-    <div>
-      {[...Array(5).keys()].map((indx) => (
-        <Icon
-          key={indx}
-          className={css.subtitleIcon}
-          iconName="icon-star"
-          size="16"
-          color={indx <= rating ? "#ffc531" : "#f2f4f7"}
-        />
-      ))}
+    <div className={css["rating-scale"]}>
+      {[...Array(5).keys()].map((indx) => {
+        const fillColor = indx <= rating ? "is-rated" : "is-blanks";
+        return (
+          <Icon
+            key={indx}
+            iconName="icon-star"
+            className={clsx(css["rating-icon"], css[fillColor])}
+          />
+        )
+      })}
     </div>
   );
 };
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired
+}
 
 export default Rating;
