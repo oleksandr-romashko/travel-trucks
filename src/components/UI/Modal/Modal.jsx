f373b055fee4936986c53a4e4ad5894a13da61ec
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-10-30 11:14:19 +0100

    Fix modal close handling

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,27 +1,60 @@
+import { useEffect } from "react";
+import PropTypes from "prop-types";
 import clsx from "clsx";
+
 import { Icon } from "@/components";
+
 import css from "./Modal.module.css";
 
 const Modal = ({ isOpen, onClose, children }) => {
-  if (!isOpen) return null;
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      e.stopPropagation();
+      if (e.key === "Escape") {
+        onClose();
+      }
+
+      console.log(e.target);
+    };
+
+    if (isOpen) {
+      document.addEventListener("keydown", handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, onClose]);
+
+  const handleModalClose = (e) => {
+    e.stopPropagation();
+    if (e.target.classList.contains('js-close-btn')) {
+      onClose();
+    }
+  }
 
   return (
-    <div className={css.modalOverlay} onClick={onClose}>
+    <div
+      className={clsx(css["modal-overlay"], "js-close-btn", "prevent-select")}
+      onClick={handleModalClose}
+    >
       <div
-        className={clsx(css.modalContent)}
-        onClick={(e) => e.stopPropagation()}
+        className={clsx(css["modal-content"])}
       >
-        {/* <div>
-          <Icon
-            iconName="icon-cross"
-            className={css.closeButton}
-            onClick={onClose}
-          />
-        </div> */}
+        <Icon
+          className={clsx(css["modal-close-btn"], "js-close-btn")}
+          iconName="icon-cross"
+        />
         {children}
       </div>
     </div>
   );
+};
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default Modal;
